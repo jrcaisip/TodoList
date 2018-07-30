f0104cab2a1b0f63c36419e9082f2fb53bbd0830
JR Caisip <juliancaisip@gmail.com>
2018-07-30 11:26:09 +0800

    working functionality and with logical folder structure

---
+++ assets/js/todos.js
@@ -0,0 +1,19 @@
+//Check Off Specific Todos by Clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+$("ul").on("click", ".delete", function(e){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	e.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(e){
+	if(e.which === 13){
+		var todoText = $(this).val();
+		$("ul").append("<li> <span class=\"delete\"><i class=\"far fa-trash-alt\"></i></span> " + todoText + "</li>");
+		$(this).val("");
+	}
+});
